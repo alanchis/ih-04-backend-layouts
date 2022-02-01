e9a8b422da3386663cfbb66f16e53bb84c760098
Alan Cruz <alanchis@hotmail.com>
2022-02-01 12:54:24 -0600

    se agregan beers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // 1. IMPORTACIONES
 const express 			= require("express")
 const app				= express()
+const hbs = require("hbs")
 
 
 // 2. MIDDLEWARES
@@ -10,6 +11,8 @@ app.use(express.static("public"))
 
 app.set("views", __dirname + "/views")
 app.set("view engine", "hbs")
+
+hbs.registerPartials(__dirname + "/views/partials")
 
 // 3. RUTAS
 
