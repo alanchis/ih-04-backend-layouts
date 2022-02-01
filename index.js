4992e50ce11755a9dc27993796108716f9f41f8d
Alan Cruz <alanchis@hotmail.com>
2022-02-01 09:47:32 -0600

    base

---
+++ index.js
@@ -0,0 +1,22 @@
+// 1. IMPORTACIONES
+const express 			= require("express")
+const app				= express()
+
+
+// 2. MIDDLEWARES
+require("dotenv").config()
+
+app.use(express.static("public"))
+
+app.set("views", __dirname + "/views")
+app.set("view engine", "hbs")
+
+// 3. RUTAS
+
+app.use("/", require("./routes/index.js"))
+
+
+// 4. SERVIDOR
+app.listen(process.env.PORT, () => {
+	console.log("Servidor activo")
+})
